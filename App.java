dbb28a24b27fa592870f4b84cb5243054c13e332
ennabow <ennabow@yandex.ru>
2025-05-16 12:27:36 +0300

    add solution for parentess sequence quiz

--- App.java
+++ App.java
@@ -1,6 +1,12 @@
 public class App {
     public static void main(String[] args) {
-        App.fizzBuss(5, 18);
+//        App.fizzBuss(5, 18);
+        App.isBalanced("()()");
+        App.isBalanced("(()())");
+        App.isBalanced(")(()())");
+        App.isBalanced("");
+        App.isBalanced(")()()(");
+        App.isBalanced("(())(");
     }
 
     public static void fizzBuss (int start, int end) {
@@ -14,5 +20,23 @@ public class App {
             var result = fizz.length() == 0 && buzz.length() == 0 ? i : fizz + buzz;
             System.out.println(result);
         }
+    }
+
+    public static boolean isBalanced (String sequence) {
+        var count = 0;
+        for (var i = 0; i < sequence.length(); i++) {
+            if (count < 0) {
+                return false;
+            }
+
+            if (sequence.charAt(i) == '(') {
+                count++;
+            } else if (sequence.charAt(i) == ')') {
+                count--;
+            }
+
+        }
+
+        return count == 0;
     }
 }
